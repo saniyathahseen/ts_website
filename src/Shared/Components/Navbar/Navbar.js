9089c05a34f0e445edf4f69380b2dd3b5c2d09cf
saniyathahseen <ayinasmadala@gmail.com>
2024-03-18 12:03:41 +0530

    COMMENT

--- src/Shared/Components/Navbar/Navbar.js
+++ src/Shared/Components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 // import { FaBars } from "react-icons/fa";
 // import { NavLink as Link } from "react-router-dom";
-import styled from "styled-components";
+// import styled from "styled-components";
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
@@ -16,46 +16,46 @@ import Navbar from 'react-bootstrap/Navbar';
 //   z-index: 12;
 // `;
 
-export const NavLink = styled(Link)`
-  color: #808080;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  cursor: pointer;
-  &.active {
-    color: #111;
-  }
-`;
+// export const NavLink = styled(Link)`
+//   color: #808080;
+//   display: flex;
+//   align-items: center;
+//   text-decoration: none;
+//   padding: 0 1rem;
+//   height: 100%;
+//   cursor: pointer;
+//   &.active {
+//     color: #111;
+//   }
+// `;
 
-export const Bars = styled(Link)`
-  display: none;
-  color: #808080;
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
-`;
+// export const Bars = styled(Link)`
+//   display: none;
+//   color: #808080;
+//   @media screen and (max-width: 768px) {
+//     display: block;
+//     position: absolute;
+//     top: 0;
+//     right: 0;
+//     transform: translate(-100%, 75%);
+//     font-size: 1.8rem;
+//     cursor: pointer;
+//   }
+// `;
 
-export const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-white-space: nowrap; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
+// export const NavMenu = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin-right: -24px;
+//   /* Second Nav */
+//   /* margin-right: 24px; */
+//   /* Third Nav */
+//   /* width: 100vw;
+// white-space: nowrap; */
+//   @media screen and (max-width: 768px) {
+//     display: none;
+//   }
+// `;
 
 const Navbars = () => {
   return (
